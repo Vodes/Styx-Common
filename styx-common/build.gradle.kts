81437300542d5eaeabbd0e6120ccaea696fe4759
Alex H <48185123+Vodes@users.noreply.github.com>
2024-03-26 23:01:51 +0100

    feat: Add cross platform logging utility

--- styx-common/build.gradle.kts
+++ styx-common/build.gradle.kts
@@ -54,6 +54,7 @@ kotlin {
         val jvmMain by getting {
             dependencies {
                 implementation(libs.ktor.client.okhttp)
+                implementation(libs.jvm.mordant)
             }
         }
     }
